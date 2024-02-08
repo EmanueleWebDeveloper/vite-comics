9d99c7e81665824d2287b1330a360c12c48d633a
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-02-08 13:08:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import FooterTop from './components/FooterTop.vue';
+import FooterMain from './components/FooterMain.vue';
+import FooterBottom from './components/FooterBottom.vue';
+import '@fontsource/pt-sans-narrow';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    FooterTop,
+    FooterMain,
+    FooterBottom
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <FooterTop />
+  <FooterMain/>
+  <FooterBottom/>
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+</style>
